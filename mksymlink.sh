9a2df7e87ce3fb12d5a14d8a02e66fab112ae5a3
yuesubi <yuesubi.zarbi@gmail.com>
2022-03-20 17:18:02 +0100

    added nitrgoen cfg

--- mksymlink.sh
+++ mksymlink.sh
@@ -16,17 +16,18 @@ files=".bashrc .xinitrc .config/picom .config/qtile"
 files="$files .config/alacritty .config/fish .config/xinits"
 files="$files .config/htop .config/gtk-2.0 .config/gtk-3.0"
 files="$files .config/dmscripts .config/awesome startwm.sh"
+files="$files .config/nitrogen"
 
 ##########
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p "$olddir"
+mkdir -p $olddir
 echo "...done"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
-cd "$dir"
+cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
@@ -34,25 +35,25 @@ for file in $files; do
     if [ ! -e $dir/$file ]; then
         parentdir=$(dirname $dir/$file)
         echo "Making directory $parentdir"
-        mkdir -p "$parentdir"
+        mkdir -p $parentdir
 
         echo "Adding $file to $dir"
-        mv -R "$home_dir/$file $dir/$file"
+        mv -R $home_dir/$file $dir/$file
     fi
 
     parentdir=$(dirname ~/$file)
     echo "Making directory $parentdir"
-    mkdir -p "$parentdir"
+    mkdir -p $parentdir
 
     parentdir=$(dirname $olddir/$file)
     echo "Making directory $parentdir"
-    mkdir -p "$parentdir"
+    mkdir -p $parentdir
 
-    if [ -f ~/$file ]; then
+    if [ -e ~/$file ]; then
         echo "Moving any existing dotfiles from ~ to $olddir"
-        mv "$home_dir/$file $olddir/$file"
+        mv $home_dir/$file $olddir/$file
     fi
     
     echo "Creating symlink to $file in home directory."
-    ln -s "$dir/$file" "$home_dir/$file"
+    ln -s $dir/$file $home_dir/$file
 done
